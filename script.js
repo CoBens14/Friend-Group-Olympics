21e09c6605a1ad4e8511078d1d1a2f2471a2483a
CoBens14 <154711139+CoBens14@users.noreply.github.com>
2024-02-24 12:12:08 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const button = document.getElementById("updatebutton")
+const div = document.getElementById("hiddenupdates")
+
+button.addEventListener("click", () => {
+    div.classList.toggle("hideupdates");
+});
+
 function yesfunction (){
     document.getElementById("imgbutton").src='winning.jpg';
 }
